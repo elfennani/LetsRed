7ee06862c1ff226956f2fe1df317e5b472f7e8d6
Nizar Elfennani <elfennani2002@gmail.com>
2024-09-01 21:11:28 +0100

    Initial commit

---
+++ app/src/main/java/com/elfen/letsred/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.elfen.letsred.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary10 = Color(0xFFFFF7ED)
+val Primary20 = Color(0xFFFED7AA)
+val Primary40 = Color(0xFFFB923C)
+val Primary60 = Color(0xFFF97316)
+val Primary80 = Color(0xFFEA580C)
+val Primary90 = Color(0xFF7C2D12)
+
+val Grey10 = Color(0xFFF8FAFC)
+val Grey20 = Color(0xFFE2E8F0)
+val Grey40 = Color(0xFF94A3B8)
+val Grey60 = Color(0xFF64748B)
+val Grey80 = Color(0xFF475569)
+val Grey90 = Color(0xFF1E293B)
+
+val Error = Color(0xFFDC2626)
+val Success = Color(0xFF4ADE80)
+val Warning = Color(0xFFFACC15)
+val BodyLight = Color(0xFF020617)
+val BodyDark = Color(0xFFf8fafc)
